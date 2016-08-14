59816805f124b5ecf8637c9f4f8b55511d075890
sunghyunzz <me@sunghyunzz.com>
2016-08-14 12:25:16 +0900

    obfuscated os string literal

--- calc.py
+++ calc.py
@@ -1,13 +1,22 @@
-getattr(__import__("os"), "write")(
+getattr(
+    __import__(
+        oct(1 << 10)[1] + dir(next)[1][6]
+    ),
+    ''.join(['w', 'r', 'i', 't', 'e'])
+)(
     1,
     (
-        lambda x: bytes(str(x), 'utf-8')
+        lambda x: bytes(str(x), 'utf8')
     )(
         eval(
             getattr(
-                __import__("sys"),
-                "argv"
-            )[1]
+                __import__(
+                    'sys'
+                ),
+                'argv'
+            )[
+                1
+            ]
         )
     )
 )
